c25f8c5569332028413c109fe4b450f5614afd68
Lazareva <o313a@odcf-worker01.inet.dkfz-heidelberg.de>
2023-04-16 10:43:35 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='GNN-based approach to transfer clonal infromation from refrerence single cell data to a ST slide',
+    author='Olga Lazareva',
+    license='MIT',
+)
