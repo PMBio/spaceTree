111fe98314048062d8ff07b3247a2757f004baa3
Lazareva <o313a@b260-pc006.inet.dkfz-heidelberg.de>
2024-09-13 12:16:17 +0200

    package and docs update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="spaceTree",  # Name of your package
-    version="0.1.3",  # Version
+    version="0.1.4",  # Version
     author="Olga Lazareva et al.",
     author_email="olazareva1993@gmail.com",
     description="PyPI package for multi-task label transfer from single-cell refrence data to spatial data",  # Short description
